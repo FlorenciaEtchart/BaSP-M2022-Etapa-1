6383bf7fef8279fdc19b2ff364e3d4b498d0b7b8
FlorenciaEtchart <floretchart04@gmail.com>
2022-04-15 14:53:39 +0200

    BaSP-week05-2022: complete-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,41 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable. 
+
+console.log ('- Exercise 1. a:');
+
+var numOne, numTwo;
+
+numOne = 1;
+numTwo = 2;
+
+var addition = numOne + numTwo;
+
+console.log  (addition);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log  ('- Exercise 1. b:');
+
+var a, b;
+
+a = 'florencia';
+b = 'etchart';
+var c = a + b;
+
+console.log (c);  
+
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log ('- Exercise 1. c:');
+
+var d , e;
+
+d = 'hello';
+e = 'earth';
+
+var f = d.length + e.length;
+
+console.log (f);
+
+
