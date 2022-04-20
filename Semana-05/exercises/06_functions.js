4f191aa2389d615995b679b8360ad0726c928c02
FlorenciaEtchart <floretchart04@gmail.com>
2022-04-20 04:08:52 +0200

    BaSP-week05-2022: completed - functions

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -1,7 +1,7 @@
 console.log('--EXERCISE 6: FUNCTIONS');
 
 /* a. Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar
-la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador. */
+la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
 
 console.log ('Exercise 6. a:');
 
