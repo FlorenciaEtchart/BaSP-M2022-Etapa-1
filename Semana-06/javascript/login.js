62c3adc4ab322967adfa6523fccae9aeb0047475
FlorenciaEtchart <floretchart04@gmail.com>
2022-04-26 10:36:36 +0200

    BaSP-W06-2022: Login screen validation was made

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,79 +1,30 @@
-window.onload = function () {
-    var email = document.getElementById('email-box');
-    var alertEmail = document.getElementsByClassName('mail-alert');
-    var password = document.getElementById('password-box');
-    var alertPassword = document.getElementsByClassName('password-alert');
-    var button = document.getElementById('log-in');
-    var backButton = document.getElementById('goback-button');
-    var emailChecked = document.getElementById('mail-checked');
-    var passwordChecked = document.getElementById('password-checked');
-    
-        email.addEventListener('blur', function(){
-        var emailInput = email.value;
-        var RegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-        if (emailInput.match(RegExp)){
-            email.classList.add('valid');
-        } else{
-            email.classList.remove('valid');
-            email.classList.add('invalid');
-            alertEmail.style.color = 'red';
-            alertEmail.innerHTML = '<span>Must enter a valid email</span>';
+window.onload  = function(){
+    var inputEmail = document.getElementById('email-box');
+    var inputPassword = document.getElementById ('password-box');
+    var messageAlert = document.getElementsByClassName ('alert-msg');
+    var regExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+    function validateEmail() {
+        if (inputEmail.value.match(regExp)) {
+            messageAlert[0].classList.add ('correct');
+            messageAlert[0].classList.remove ('invalid');
+            messageAlert[0].innerHTML = 'Valid email';
+        } else if (!inputEmail.value.match (regExp)) {
+            messageAlert[0].classList.add ('invalid');
+            messageAlert[0].classList.remove ('correct');
+            messageAlert[0].innerHTML = 'Must enter a valid email';
         }
-    });
-
-    email.addEventListener('focus', function () {
-        alertEmail.classList.remove('invalid');
-        alertEmail.classList.remove('valid');
-        alertEmail.style.color = "white";
-    });
-
-    password.addEventListener('blur', function(){
-        var allowed = '0123456789abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
-        var numbers = '0123456789';
-        var letters = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
-        var passwordValue = password.value;
-        var isAlphanumeric = true;
-        var onlyNumbers = true;
-        var onlyLetters = true;
-    
-    passwordValue.split('').forEach(letter=>{
-        if (allowed.indexOf(letter) == -1) isAlphanumeric = false;
-        if (numbers.indexOf(letter) == -1) onlyNumbers = false;
-        if (letters.indexOf(letter) == -1) onlyLetters = false;
-    });
-    
-    if (isAlphanumeric && !onlyNumbers && !onlyLetters){
-        password.classList.add('valid');
-    } else {
-        password.classList.add('invalid');
-        alertPassword.style.color = '#ff0000';
-        alertPassword.innerHTML = '<span>Must enter a valid password</span>';
     }
     
-    password.addEventListener('focus', function(){
-        alertPassword.classList.remove('invalid');
-        alertPassword.classList.remove('valid');
-        alertPassword.style.color = "white";
-    });
+    inputEmail.onblur = function() {
+        validateEmail();
+    }
 
-    button.addEventListener('click', function(){
-        var emailValue = email.value;
-        var passwordValue = password.value;
-        document.getElementById('overlay').style.display = 'block';
-        if (email.classList.contains('valid')){
-            emailChecked.innerHTML = '<span>Email: </span>' + emailValue;
-        } else if (email.classList.contains('invalid')){
-            emailChecked.innerHTML = '<span>Email: Incorrect email</span>';
-        };
+    inputEmail.onfocus = function() {
+        messageAlert[0].classList.remove ('correct');
+        messageAlert[0].classList.add ('invalid');
+        messageAlert[0].innerHTML = '';
+    }
 
-        if (password.classList.contains('valid')){
-            passwordChecked.innerHTML = '<span>Password: </span>' + passwordValue;
-        }else if (password.classList.contains('invalid')){
-            passwordChecked.innerHTML = '<span>Password: Incorrect password</span>';
-        };
-    });
-
-    backButton.addEventListener('click', function(){
-        document.getElementById('overlay').style.display = 'none';
-    });
 }
+
